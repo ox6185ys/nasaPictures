11cd9afe927e4eb0329300494b338471d4c5fcdc
Patrick <ox6185ys@go.minneapolis.edu>
2017-04-13 19:36:19 -0500

    Astropix Favorites

--- app.js
+++ app.js
@@ -4,26 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var hbs = require('express-handlebars')
+var hbs = require('express-handlebars');
 var session = require('express-session');
 var MongoDBStore = require('connect-mongodb-session')(session);
 
 var index = require('./routes/index');
-//does ./routes/users get commented out?  Slide 27
-var users = require('./routes/users');
-var favorites = require(.
-/routes/
-favorites
-)
-;
+var favorites = require('./routes/favorites');
 
 var app = express();
 
 // view engine setup
 app.engine('.hbs', hbs({
-    extname: '.hbs',
-    defaultLayout: 'layout',
-    partialsDir: path.join(__dirname, 'views/partials')
+    extname:'.hbs',
+    defaultLayout: 'layout'
 }));
 
 app.set('views', path.join(__dirname, 'views'));
@@ -33,45 +26,41 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Configure sessions, and session storage in MongoDB
+// Configure sessions, and session storage in MongoDB
 var mongo_pw = process.env.MONGO_PW;
 var url = 'mongodb://admin:' + mongo_pw + '@localhost:27017/helloSessions?authSource=admin';
 
 var store = new MongoDBStore({
-    uri: url,
-    collection: 'sessions'
-}, function (error) {
-    // todo deal with error connection
+    uri : url,
+    collection : 'sessions'
+}, function(error) {
+    // todo deal with error connection
     if (error) console.log(error)
 });
+
 app.use(session({
     secret: "put a random string here",
     resave: false,
     saveUninitialized: false,
     store: store
 }));
+
 app.use('/', index);
 app.use('/favorites', favorites);
-}
-
-
-//do the 2 app.use below (index and users) need to be removed?
-app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/favorites.js
+++ routes/favorites.js
@@ -1,32 +1,33 @@
-var express = require('express');
+var express = require('express');
 var router = express.Router();
 
-/* GET favorites page. */
+
+/* GET favorites page. */
 router.get('/', function(req, res, next) {
-      res.render('favorites', {favorites : req.session.favorites});
+    res.render('favorites', {favorites : req.session.favorites});
 });
 
-/* POST Add new favorite */
+
+/* POST Add new favorite */
 router.post('/add', function(req, res, next){
 
-    // If no favorites array, create one
+    // If no favorites array, create one
     if (!req.session.favorites) {
-        req.session.favorites = [] ; //Create empty array  }
+        req.session.favorites = [] ; //Create empty array
     }
-}
 
-//Check if this image is already in array
+    //Check if this image is already in array
     for (var x = 0 ; x < req.session.favorites.length ; x++) {
-    if (req.session.favorites[x].date == req.body.date) {
-        console.log('This is already a favorite');
-        return res.redirect('back');   // Back to previous page    }  }
-    }
+        if (req.session.favorites[x].date == req.body.date) {
+            console.log('This is already a favorite');
+            return res.redirect('back');   // Back to previous page
+        }
     }
 
-    // If not, add to array and redirect to favorites page
-    req.session.favorites.push(req.body);
-    res.redirect('/favorites');
+    // If not, add to array and redirect to favorites page
+    req.session.favorites.push(req.body);
+    res.redirect('/favorites');
 
-});
+});
 
 module.exports = router;
